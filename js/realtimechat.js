5bae70db86b397c5cf6c8a38f788050abefa6aaf
Swissforce <martin.dueppenbecker@gmail.com>
2022-03-20 20:44:10 +0100

    Verschiede States möglich & Problembehebung bei mehreren Chats auf einmal

--- js/realtimechat.js
+++ js/realtimechat.js
@@ -4,6 +4,30 @@
  *
  */
 
+//Festsetzung der benutzerdefinierten Konstanten
+
+const CHATSERVER = "127.0.0.1";
+
+
+//Festsetzung anderer Konstanten & Variablen
+
+const seiteLogin = 1;
+const seiteAuswahl = 2;
+const seiteChat = 3;
+
+
+var aktuelleSeite;
+var chatDiv;
+var chatDivHeader;
+var chatDivContent;
+var chatSchliessen;
+var chatZurueck;
+var chatTestButton;
+var chatDivContent;
+//Map mit allen Chats. So kann man sehen, welchem Chat was gehört
+var chatMap = new Map();
+
+
 
 //Funktionen
 
@@ -15,36 +39,182 @@ var anzGespawnterChats = 0;
 function spawnChat(){
     anzGespawnterChats++;
 
-    var chatDiv = document.createElement('div');
+    chatDiv = document.createElement('div');
     chatDiv.className = "chatDiv";
     chatDiv.id = anzGespawnterChats;
 
-
-    var chatDivHeader = document.createElement('div');
+    chatDivHeader = document.createElement('div');
     chatDivHeader.className = "chatDivHeader";
     chatDivHeader.textContent = "Realtime-Chat Nr." + anzGespawnterChats;
 
-    var chatSchliessen = document.createElement('button');
+    chatSchliessen = document.createElement('button');
     chatSchliessen.className = "chatSchliessen";
     chatSchliessen.innerHTML = '<img src="icons/close.png" height=10 width=10>';
-
     chatSchliessen.addEventListener('click', () => {
         destroyChat(chatDiv.id);
     })
 
+    chatZurueck = document.createElement('button');
+    chatZurueck.className = "chatZurueck";
+    chatZurueck.innerHTML = '<img src="icons/back.png" height=10 width=10>';
+    chatZurueck.addEventListener('click', () => {
+        neueSeite(aktuelleSeite - 1, chatDiv.id);
+    });
 
-    var text = document.createElement('p');
-    text.textContent = "Ich bin ein Chat";
+
+
+    chatTestButton = document.createElement('button');
+    chatTestButton.textContent = "test";
+    chatTestButton.addEventListener('click', () => {
+        neueSeite(2, chatDiv.id);
+    });
+
+
+    chatDivContent = document.createElement('div');
+    chatDivContent.className = "chatDivContent";
+
+    aktuelleSeite = seiteLogin;
 
 
     chatDivHeader.appendChild(chatSchliessen);
+    chatDivHeader.appendChild(chatZurueck);
+    chatDivHeader.appendChild(chatTestButton);
     chatDiv.appendChild(chatDivHeader);
-    chatDiv.appendChild(text);
+    chatDiv.appendChild(chatDivContent);
 
     document.body.appendChild(chatDiv);
 
 
+    updateVars(anzGespawnterChats);
+
+
+    neueSeite(seiteLogin, anzGespawnterChats);
+
     addDragElement(chatDiv);
+
+}
+
+/**
+ * Ladet die Variablen aus der Map in die des Programmes ein
+ */
+function loadVars(id){
+    var chatObj = chatMap.get(id);
+
+    this.chatDiv = chatObj.chatDiv;
+    this.chatDivHeader = chatObj.chatDivHeader;
+    this.chatSchliessen = chatObj.chatSchliessen;
+    this.chatZurueck = chatObj.chatZurueck;
+    this.chatTestButton = chatObj.chatTestButton;
+    this.chatDivContent = chatObj.chatDivContent;
+    this.aktuelleSeite = chatObj.aktuelleSeite;
+}
+
+/**
+ * Ladet die Variablen des Programmes in die der Map ein
+ */
+function updateVars(id){
+    var chatObj = {
+        chatDiv:chatDiv,
+        chatDivHeader:chatDivHeader,
+        chatSchliessen:chatSchliessen,
+        chatZurueck:chatZurueck,
+        chatTestButton:chatTestButton,
+        chatDivContent:chatDivContent,
+        aktuelleSeite:aktuelleSeite
+    };
+
+    chatMap.set(id, chatObj);
+}
+
+/**
+ * Ändert den Zustand des Chatfensters.
+ * Siehe konstanten oben definiert
+ */
+function neueSeite(seiteNeu, id){
+    loadVars(Number(id));
+
+    //Löscht den Content des Fensters (ausser Header)
+    while(chatDivContent.hasChildNodes()){
+        chatDivContent.removeChild(chatDivContent.lastChild);
+    }
+
+    chatZurueck.style = "display: inline";
+
+    //Muss erneuert werden
+    chatZurueck.addEventListener('click', () => {
+        neueSeite(aktuelleSeite, chatDiv.id);
+    });
+
+
+
+    switch (seiteNeu) {
+        case seiteLogin:
+            loginSeite();
+            chatZurueck.style = "display: none";
+            break;
+
+        case seiteAuswahl:
+            auswahlSeite();
+            break;
+
+        default:
+            console.log("Es ist ein Fehler aufgetreten!");
+            break;
+    }
+
+
+    aktuelleSeite = seiteNeu;
+
+    updateVars(Number(id));
+
+
+
+
+
+    function loginSeite(){
+        var loginForm =  document.createElement('form');
+        loginForm.id = "loginForm";
+
+        var emailTitel = document.createElement('p');
+        emailTitel.textContent = "Email";
+
+        var emailInput = document.createElement('input');
+        emailInput.type = "email";
+        emailInput.required = true;
+        emailInput.autofocus = true;
+
+        var passwortTitel = document.createElement('p');
+        passwortTitel.textContent = "Passwort";
+
+        var passwortInput = document.createElement('input');
+        passwortInput.type = "password";
+        passwortInput.required = true;
+
+        var loginButton = document.createElement('button');
+        loginButton.type = "submit";
+        loginButton.textContent = "Login";
+
+
+        chatDivContent.appendChild(emailTitel);
+        chatDivContent.appendChild(emailInput);
+
+        chatDivContent.appendChild(passwortTitel);
+        chatDivContent.appendChild(passwortInput);
+
+        chatDivContent.appendChild(document.createElement('br'));
+        chatDivContent.appendChild(document.createElement('br'));
+        chatDivContent.appendChild(loginButton);
+
+    }
+
+
+    function auswahlSeite(){
+        //TODO
+        var emailTitel = document.createElement('p');
+        emailTitel.textContent = "test";
+
+        chatDivContent.appendChild(emailTitel);
+    }
 }
 
 /**
@@ -58,16 +228,11 @@ function destroyChat(id){
 /**
  * Bewegt den angeklickten Chat in Vordergrund
  */
-var previousForegroundElement;
 function moveElementIntoForeground(){
-    if (previousForegroundElement === undefined){
-        this.style.zIndex = 1;
-    }
-    else {
-        var zIndex = Number(previousForegroundElement.style.zIndex) + 1;
-        this.style.zIndex = zIndex;
-    }
-    previousForegroundElement = this;
+    document.body.removeChild(this);
+    document.body.appendChild(this);
+
+    loadVars(Number(this.id));
 }
 
 
@@ -79,7 +244,7 @@ function addAllDragElements(elements){
 
 //https://www.w3schools.com/howto/howto_js_draggable.asp
 function addDragElement(element) {
-    element.addEventListener("click", moveElementIntoForeground);
+    element.addEventListener('mousedown', moveElementIntoForeground);
 
     var pos1 = 0, pos2 = 0, pos3 = 0, pos4 = 0;
 
@@ -157,10 +322,12 @@ function showChatButton(){
 
 
 
-//Ausführen beim Laden der Webseite
 
+//Ausführen beim Laden der Webseite
 
 
 showChatButton();
 
 addAllDragElements(document.getElementsByClassName("chatDiv"));
+
+
